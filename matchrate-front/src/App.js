0046fa18bc7a7423a9de07bebb47df700dd0fcdf
AlexAmI123 <alex.majka@zendesk.com>
2023-02-20 11:44:27 +0000

    Initial Commit

---
+++ matchrate-front/src/App.js
@@ -0,0 +1,27 @@
+import { Routes, Route, Link, Switch} from 'react-router-dom'
+import Match from './pages/match/Match';
+import Matches from './pages/matches/Matches';
+import { Container, Navbar } from 'react-bootstrap';
+
+  
+function App() {
+    return (
+      <>
+        <Container>
+          {/* <Navbar expand="lg" variant="light" bg="light">
+            <Container>
+              <Link className="nav-link" to="/">Matches</Link>
+              <Link className="nav-link" to="/Match">Match</Link>
+            </Container>
+            </Navbar><br /> 
+          */}
+        
+          <Routes>
+            <Route path="/" element={<Matches />} />
+            <Route path="/Matches/:slug" element={<Match />} />
+          </Routes>
+        </Container>
+      </>
+    );
+  }
+export default App;
